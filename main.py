1553bf806282009c69f24bbe38d0d5ab6254ee12
Ellenor <f.taghayor@gmail.com>
2021-08-14 19:48:48 +0200

    basic sample finished with lots of help

--- main.py
+++ main.py
@@ -15,8 +15,10 @@ bot.
 """
 
 import logging
-
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+import requests
+from typing import Dict
+from bs4 import BeautifulSoup
+from telegram import ReplyKeyboardMarkup, Update, ReplyKeyboardRemove
 from telegram.ext import (
     Updater,
     CommandHandler,
@@ -33,104 +35,61 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-GENDER, PHOTO, LOCATION, BIO = range(4)
+CHOOSING, TYPING_CHOICE = range(2)
+
+# reply_keyboard = [
+#     ['Age', 'Favourite colour'],
+#     ['Number of siblings', 'Something else...'],
+#     ['Done'],
+# ]
+# markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+
+def facts_to_str(user_data: Dict[str, str]) -> str:
+    """Helper function for formatting the gathered user info."""
+    facts = [f'{key} - {value}' for key, value in user_data.items()]
+    return "\n".join(facts).join(['\n', '\n'])
 
 
 def start(update: Update, context: CallbackContext) -> int:
-    """Starts the conversation and asks the user about their gender."""
-    reply_keyboard = [['Boy', 'Girl', 'Other']]
-
+    """Start the conversation and ask user for input."""
     update.message.reply_text(
-        'Hi! My name is Professor Bot. I will hold a conversation with you. '
-        'Send /cancel to stop talking to me.\n\n'
-        'Are you a boy or a girl?',
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder='Boy or Girl?'
-        ),
+        "Hi! My name is Lela. Write down any word and I'll tell you its gender! Try to avoid typos."
     )
 
-    return GENDER
+    return TYPING_CHOICE
 
 
-def gender(update: Update, context: CallbackContext) -> int:
-    """Stores the selected gender and asks for a photo."""
-    user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
+def answer(update: Update, context: CallbackContext) -> int:
+    """Ask the user for a description of a custom category."""
+    user_data = context.user_data
+    text = update.message.text
+    url = 'https://www.wordreference.com/fren/{}'
+    r = requests.get(url.format(text))
+    if(r.status_code == 200):
+        soup = BeautifulSoup(r.content, features="lxml")
+        all_results = soup.select("em.POS2.tooltip")
+        res = all_results[0]
+
+        update.message.reply_text(
+            'le {}'.format(text) if(list(res)[0]) == 'nm' else 'la {}'.format(text)
+        )
+
+    return TYPING_CHOICE
+
+
+def done(update: Update, context: CallbackContext) -> int:
+    """Display the gathered info and end the conversation."""
+    user_data = context.user_data
+    if 'choice' in user_data:
+        del user_data['choice']
+
     update.message.reply_text(
-        'I see! Please send me a photo of yourself, '
-        'so I know what you look like, or send /skip if you don\'t want to.',
+        f"I learned these facts about you: {facts_to_str(user_data)}Until next time!",
         reply_markup=ReplyKeyboardRemove(),
     )
 
-    return PHOTO
-
-
-def photo(update: Update, context: CallbackContext) -> int:
-    """Stores the photo and asks for a location."""
-    user = update.message.from_user
-    photo_file = update.message.photo[-1].get_file()
-    photo_file.download('user_photo.jpg')
-    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
-    update.message.reply_text(
-        'Gorgeous! Now, send me your location please, or send /skip if you don\'t want to.'
-    )
-
-    return LOCATION
-
-
-def skip_photo(update: Update, context: CallbackContext) -> int:
-    """Skips the photo and asks for a location."""
-    user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
-    update.message.reply_text(
-        'I bet you look great! Now, send me your location please, or send /skip.'
-    )
-
-    return LOCATION
-
-
-def location(update: Update, context: CallbackContext) -> int:
-    """Stores the location and asks for some info about the user."""
-    user = update.message.from_user
-    user_location = update.message.location
-    logger.info(
-        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
-    )
-    update.message.reply_text(
-        'Maybe I can visit you sometime! At last, tell me something about yourself.'
-    )
-
-    return BIO
-
-
-def skip_location(update: Update, context: CallbackContext) -> int:
-    """Skips the location and asks for info about the user."""
-    user = update.message.from_user
-    logger.info("User %s did not send a location.", user.first_name)
-    update.message.reply_text(
-        'You seem a bit paranoid! At last, tell me something about yourself.'
-    )
-
-    return BIO
-
-
-def bio(update: Update, context: CallbackContext) -> int:
-    """Stores the info about the user and ends the conversation."""
-    user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('Thank you! I hope we can talk again some day.')
-
-    return ConversationHandler.END
-
-
-def cancel(update: Update, context: CallbackContext) -> int:
-    """Cancels and ends the conversation."""
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    update.message.reply_text(
-        'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
-    )
-
+    user_data.clear()
     return ConversationHandler.END
 
 
@@ -143,19 +102,31 @@ def main() -> None:
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            GENDER: [MessageHandler(Filters.regex('^(Boy|Girl|Other)$'), gender)],
-            PHOTO: [MessageHandler(Filters.photo, photo), CommandHandler('skip', skip_photo)],
-            LOCATION: [
-                MessageHandler(Filters.location, location),
-                CommandHandler('skip', skip_location),
+            CHOOSING: [
+                MessageHandler(
+                    Filters.text & ~(Filters.command | Filters.regex(
+                        '^Done$')), answer
+                )
             ],
-            BIO: [MessageHandler(Filters.text & ~Filters.command, bio)],
+            TYPING_CHOICE: [
+                MessageHandler(
+                    Filters.text & ~(Filters.command | Filters.regex(
+                        '^Done$')), answer
+                )
+            ],
+            # TYPING_REPLY: [
+            #     MessageHandler(
+            #         Filters.text & ~(Filters.command |
+            #                          Filters.regex('^Done$')),
+            #         received_information,
+            #     )
+            # ],
         },
-        fallbacks=[CommandHandler('cancel', cancel)],
+        fallbacks=[MessageHandler(Filters.regex('^Done$'), done)],
     )
 
     dispatcher.add_handler(conv_handler)
